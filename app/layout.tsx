3166f8662d32d5aaea7da21c28df5ddbe4464e97
chasingzen <jimmy.starling@gmail.com>
2025-04-04 11:49:59 -0400

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css';
 
+export const metadata = {
+  title: 'EchoTrace',
+  description: 'Decoding Speech. Empowering Insight.',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
