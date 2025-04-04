ae733aa9aa08c77ad5b19813b3e146289aa6a525
chasingzen <jimmy.starling@gmail.com>
2025-04-04 11:49:45 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulseGlow: "pulse 3s ease-in-out infinite"
+      }
+    },
   },
   plugins: [],
 }
